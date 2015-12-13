8362807272bd97aa23b3c8b11f734bb1e859e9dc
Maxym Sholomko <maxym.sholomko@gmail.com>
2015-12-14 01:14:38 +0200

    Homework3-4

--- GoIT/Markup/Home/lesson_JS_34/js/script.js
+++ GoIT/Markup/Home/lesson_JS_34/js/script.js
@@ -4,15 +4,20 @@ var body = document.querySelector('body');
    body.appendChild(wrapper);
 
 var create = {
-   createElement: function(tagName, className, content){
+   createElement: function(tagName, className, inputType, content){
       var element = document.createElement(tagName);
+      if (element.tagName === 'input'){
+         element.setAttribute('inputType', element.inputType);
+      }
       element.className = className;
       element.innerHTML = content;
+      
    return element;
    }
 }
-var head = create.createElement('h3', 'h3', 'Тест по програмированнию');
-var input = create.createElement('input', 'button', 'Проверить мои результаты');
+var head = create.createElement('h3', 'h3', '', 'Тест по програмированнию');
+var input = create.createElement('input', 'button', 'submit', 'Проверить мои результаты');
 
 wrapper.appendChild(head);
 wrapper.appendChild(input);
+
