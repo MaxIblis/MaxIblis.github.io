f87c91f6fd438288575f4d81c1ca867621b1f65e
MaxIblis <maxym.sholomko@gmail.com>
2016-01-13 15:55:18 +0200

    Work56_v1.2

--- GoIT/Markup/Home/lesson_JS_56/js/script.js
+++ GoIT/Markup/Home/lesson_JS_56/js/script.js
@@ -7,7 +7,7 @@ function create (newElement) {
 
 var divTimeTable = create(divTimeTable);
 var divStart = create(divStart);
-var split = create(split);
+// var divSplit = create(divSplit);
 
 var timeTable = document.createElement('div');
 divTimeTable.appendChild(timeTable);
@@ -15,18 +15,67 @@ timeTable.innerHTML = '00:00:00:000';
 timeTable.style.fontSize = "48px";
 timeTable.classList.add('col-md-2', 'col-md-offset-5', 'text-center');
 
-var start = document.createElement('button');
-divStart.appendChild(start);
-start.classList.add('btn', 'btn-success', 'col-md-2', 'col-md-offset-4');
-start.setAttribute('type', 'button');
-start.innerHTML = 'Start';
-start.style.fontSize = "28px";
-start.addEventListener("click", startCount);
+var buttonStart = document.createElement('button');
+divStart.appendChild(buttonStart);
+buttonStart.classList.add('btn', 'btn-success', 'col-md-2', 'col-md-offset-4');
+buttonStart.setAttribute('type', 'button');
+buttonStart.innerHTML = 'Start';
+buttonStart.style.fontSize = "28px";
+buttonStart.addEventListener("click", startCount);
 
-var reset = document.createElement('button');
-divStart.appendChild(reset);
-reset.classList.add('btn', 'btn-warning', 'col-md-1');
-reset.setAttribute('type', 'button');
-start.innerHTML = 'Reset';
-reset.style.fontSize = "28px";
-reset.addEventListener("click", resetCount);
+var buttonReset = document.createElement('button');
+divStart.appendChild(buttonReset);
+buttonReset.classList.add('btn', 'btn-success', 'col-md-2');
+buttonReset.setAttribute('type', 'button');
+buttonReset.innerHTML = 'Reset';
+buttonReset.style.fontSize = "28px";
+// buttonReset.addEventListener("click", resetCount);
+
+var zero = new Date (0, 0),
+seconds = 0,
+minutes = 0,
+hours = 0,
+timer,
+time;
+
+function counter(){
+   zero.setMilliseconds(zero.getMilliseconds() + 4);
+   var milliseconds = zero.getMilliseconds();
+   var millisecondsValue, secondsValue, minutesValue, hoursValue;
+      if (milliseconds >= 996){
+         seconds++;
+      }
+      if (seconds >= 60){
+         seconds = 0;
+         minutes++;
+      }
+      if (minutes >= 60) {
+         minutes = 0;
+         hours++;
+      }
+
+      if (milliseconds < 10){
+         millisecondsValue = '00' + milliseconds;
+      }else if(milliseconds < 100) {
+            millisecondsValue = '0' + milliseconds;
+         }
+      else {
+            millisecondsValue = milliseconds;
+         }
+
+      secondsValue = seconds;
+      minutesValue = minutes;
+      hoursValue = hours;
+
+      time = hoursValue + ":" + minutesValue + ":" + secondsValue + ":" + millisecondsValue;
+      timeTable.innerHTML = time;
+}
+
+function startCount(){
+   buttonStart.classList.remove('btn-success');
+   buttonStart.classList.add('btn-danger');
+   buttonStart.innerHTML = 'Pause';
+   timer = setInterval (counter, 4);
+   buttonStart.removeEventListener("click", startCount);
+   // button.addEventListener("click", pauseCount);
+}
