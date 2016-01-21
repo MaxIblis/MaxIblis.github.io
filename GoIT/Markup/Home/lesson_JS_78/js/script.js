757171545718f45b93ec42971c54c1ecec2531c8
Maxym Sholomko <maxym.sholomko@gmail.com>
2016-01-22 01:02:24 +0200

    Home5\6v1.7

--- GoIT/Markup/Home/lesson_JS_78/js/script.js
+++ GoIT/Markup/Home/lesson_JS_78/js/script.js
@@ -1,4 +1,21 @@
-$(function() {
- });
+$(function () {
 
- 
+var $links = $('.menu a');
+var $active = $('.menu a.is-active');
+
+
+$links.on('click', function(e){
+	e.preventDefault();
+	$active.removeClass('is-active');
+	var $act = $(this).addClass("is-active");
+	$active = $(this);
+	var $click = $active.attr('rel');
+})
+
+
+
+
+})
+
+
+
