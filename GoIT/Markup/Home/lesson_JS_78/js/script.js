d4ab1784c9edcab6f34a35529837a7ef78ec710d
Maxym Sholomko <maxym.sholomko@gmail.com>
2016-01-26 04:10:06 +0200

    Home78v2.1

--- GoIT/Markup/Home/lesson_JS_78/js/script.js
+++ GoIT/Markup/Home/lesson_JS_78/js/script.js
@@ -1,24 +1,47 @@
 $(function () {
 
-var $links = $('.menu a');
-var $active = $('.menu a.is-active');
-var $divActive = $('.text');
+	var $links = $('.menu a');
+	var $active = $('.menu a.is-active');
+	var $divActive = $('.text__shown');
 
-$links.on('click', function(e){
-	e.preventDefault();
-	$active.removeClass('is-active');
-	var $act = $(this).addClass("is-active");
-	$active = $(this);
-	var $click = $active.attr('rel');
-	console.log($click);
-	$divActive.css('display', 'none');
-	var $div = $("#"+$click).css('display','block');
-	$div = $divActive;
+	$links.on('click', 
+		function(e){
+			e.preventDefault();
+			$active.removeClass('is-active');
+			var $act = $(this).addClass("is-active");
+			$active = $act;
+			var $click = $active.attr('rel');
+			$divActive.removeClass('text__shown');
+			var $div = $("#"+$click).addClass('text__shown');
+			$divActive = $div;
+		}
+	);
 
-})
+	$('input').hover(showText, hideText);
 
+	function showText(){
+			$(this).parent().find('.show_hint').remove();
+			var $hint = $(this).attr('title');
+			$(this).parent().append("<p class='show_hint'>" + $hint + "</p>");
+	}; 	
+	
+	function hideText(){
+			$(this).parent().find('.show_hint').remove();
+	};
 
+	$('.info').on('click', 
+		function(){
+			$('.form input').each(showText);
+		});
 
+	// function allHints(){
+	// 	$('.form input').each(showText);
+	// };
+	// function allHintsHide(){
+	// 	$('.form input').each(hideText);
+	// };
+
+	// $('.info').toggle(allHints, allHintsHide);
 
 })
 
